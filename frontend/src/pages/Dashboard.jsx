dee85c0c379fd66b58eaa2a0f17a5a41d4877456
João Azevedo <azevedojoao05@gmail.com>
2026-02-13 21:57:26 +0000

    log in register

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+// src/pages/Dashboard.jsx
+export default function Dashboard() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Dashboard</h1>
+      <p>Ainda em construção...</p>
+    </div>
+  );
+}
